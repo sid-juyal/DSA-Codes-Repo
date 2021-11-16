2593b4c0f4e01dc754afbfcea82e7bd870d66b30
Siddharth Juyal <65865527+sid-juyal@users.noreply.github.com>
2021-11-16 18:37:54 +0530

    Add files via upload

---
+++ mid_node.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node*next;
+    node(int d)
+    {
+        data=d;
+        next=NULL;
+    }
+};
+void insertAtHead(node*&head,int d)
+{
+    if(head==NULL)
+    {
+        head=new node(d);
+        return;
+    }
+    node*n=new node(d);
+    n->next=head;
+    head=n;
+}
+void print(node*head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+}
+node*mid_node(node*head)
+{
+    if(head==NULL || head->next==NULL)
+    {
+        return head;
+    }
+    node*slow=head;
+    node*fast=head->next;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    return slow;
+}
+int main()
+{
+    node*head=NULL;
+    insertAtHead(head,5);
+    insertAtHead(head,4);
+    insertAtHead(head,3);
+    insertAtHead(head,2);
+    insertAtHead(head,1);
+    print(head);
+    cout<<endl;
+    node*mid=mid_node(head);
+    cout<<mid->data;
+    return 0;
+}
