8de942f99d181ce91402df428f568d6377cdd800
Siddharth Juyal <65865527+sid-juyal@users.noreply.github.com>
2021-12-21 23:15:33 +0530

    Add files via upload

---
+++ Fibonacci_Bottom_Up_DP.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int fib(int n)
+{
+    int dp[1000]={0};
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+int spaced_optimize(int n)
+{
+    if(n==0 || n==1)
+    {
+        return n;
+    }
+    int a=0;
+    int b=1;
+    int c;
+    for(int i=2;i<=n;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return c;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int k=fib(n);
+    cout<<"Ans :"<<k<<endl;
+    cout<<"Space Optimized result: "<<spaced_optimize(n);
+    return 0;
+}
